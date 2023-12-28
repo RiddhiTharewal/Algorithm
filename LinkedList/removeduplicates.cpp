916b61131b4dc4f2e0865dd14b7d533764eb57ea
Riddhi Tharewal <69417801+RiddhiTharewal@users.noreply.github.com>
2023-12-29 00:04:57 +0530

    Create removeduplicates.cpp

---
+++ LinkedList/removeduplicates.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(!head){
+            return head;
+        }
+        ListNode* prev = head;
+        ListNode* curr = head->next;
+        while(curr){
+            if(curr->val==prev->val){
+                curr = curr->next;
+                prev->next=curr;
+            }else{
+                curr = curr->next;
+                prev=prev->next;
+            }
+        }
+        return head;
+    }
+};

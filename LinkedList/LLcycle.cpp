c53d655291f79a1449cabc21e280835be012184f
Riddhi Tharewal <69417801+RiddhiTharewal@users.noreply.github.com>
2023-12-29 12:50:01 +0530

    Create LLcycle.cpp

---
+++ LinkedList/LLcycle.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+/*class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+	
+		// making two pointers fast and slow and assignning them to head
+        ListNode *fast = head;
+        ListNode *slow = head;
+        
+		// till fast and fast-> next not reaches NULL
+		// we will increment fast by 2 step and slow by 1 step
+        while(fast != NULL && fast ->next != NULL)
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+            
+			
+			// At the point if fast and slow are at same address
+			// this means linked list has a cycle in it.
+            if(fast == slow)
+                return true;
+        }
+        
+		// if traversal reaches to NULL this means no cycle.
+        return false;
+    }
+};
+*/
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(!head){
+            return false;
+        }
+        vector<ListNode*> a;
+        vector<ListNode*>::iterator it;
+        a.push_back(head);
+        head=head->next;
+        while(head){
+            it = find(a.begin(),a.end(), head);
+            if(it != a.end()){
+                return true;
+            }
+            a.push_back(head);
+            head=head->next;
+        }
+        return false;
+    }
+};
